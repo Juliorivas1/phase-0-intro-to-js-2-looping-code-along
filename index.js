cbd9e066d403ae195eb212c1ea1db9796248a268
Julio Rivas <rivasjulio@ymail.com>
2023-02-12 15:07:10 -0500

    DONE

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(arr, birthday) {
+    for (let i = 0; i < arr.length; i++)   {
+  const messages =  [
+        
+            "Thank you, Guadalupe, for the wonderful surprise gift!",
+            "Thank you, Ollie, for the wonderful surprise gift!",
+            "Thank you, Aki, for the wonderful surprise gift!",
+
+        ]
+        return messages;
+
+    }
+}
+console.log(writeCards(cards, "surprise"));
+
+function countDown(number) {
+while (number > -1 ) {
+console.log(number --)
+}
+}
+
+
+
+
+
+
